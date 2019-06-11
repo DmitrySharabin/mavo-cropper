daec78bc65eedebebe07ce264892ce1771df2bb3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2019-06-12 01:04:05 +0300

    Add toolbar

--- mavo-cropper.js
+++ mavo-cropper.js
@@ -31,33 +31,20 @@
 			} else {
 				options = defaults;
 			}
-
 		},
 
 		editor: function () {
 			let fileName;
+
 			Mavo.setAttributeShy(this.element, 'mv-uploads', 'images');
 			// Generate the default editor
 			const popup = this.createUploadPopup('image/*', 'image', 'png');
-			// Do I have to unbind to change the event handler, or there is another way?
-			// What about other events?
-			$.unbind($('input[type=file]', popup), 'change');
-			$('input[type=file]', popup).addEventListener('change', evt => {
-				const file = evt.target.files[0];
 
-				if (file) {
-					fileName = file.name;
-					const tempURL = URL.createObjectURL(file);
-					this.element.setAttribute(this.attribute, tempURL);
-					if (this.cropper) {
-						this.cropper.replace(tempURL);
-					}
-				}
-			});
 			// and extend it with appropriate elements
 			$.create('div', {
 				// Wrap the image element with a block element (container)
 				className: 'cropper-wrapper',
+				// Do we need style it here?
 				style: {
 					maxWidth: this.element.offsetWidth + 'px'
 				},
@@ -72,13 +59,19 @@
 				}],
 				inside: popup
 			});
+
+			// Create the cropper
+			this.cropper = new Cropper($('.cropper-preview', popup), options);
+
 			// TODO: Generate the cropper-bar depending on the Cropper options: this.cropper.options
 			$.create('div', {
 				className: 'cropper-bar',
 				contents: {
 					tag: 'button',
+					className: 'cropper-upload',
 					type: 'button',
-					textContent: 'Done',
+					textContent: this.mavo._('cropper-upload'),
+					title: this.mavo._('cropper-upload'),
 					events: {
 						click: function () {
 							this.cropper.getCroppedCanvas().toBlob(file => {
@@ -89,14 +82,115 @@
 				},
 				inside: popup
 			});
-			this.cropper = new Cropper($('.cropper-preview', popup), options);
+
+			// Rotate
+			if (this.cropper.options.rotatable) {
+				$.create('button', {
+					type: 'button',
+					className: 'cropper-rotate-left',
+					textContent: this.mavo._('cropper-rotate-left'),
+					title: this.mavo._('cropper-rotate-left'),
+					events: {
+						click: () => {
+							this.cropper.rotate(-90);
+						}
+					},
+					inside: $('.cropper-bar', popup)
+				});
+				$.create('button', {
+					type: 'button',
+					className: 'cropper-rotate-right',
+					textContent: this.mavo._('cropper-rotate-right'),
+					title: this.mavo._('cropper-rotate-right'),
+					events: {
+						click: () => {
+							this.cropper.rotate(90);
+						}
+					},
+					inside: $('.cropper-bar', popup)
+				});
+			}
+
+			// Flip
+			if (this.cropper.options.scalable) {
+				$.create('button', {
+					type: 'button',
+					className: 'cropper-flip-horizontal',
+					textContent: this.mavo._('cropper-flip-horizontal'),
+					title: this.mavo._('cropper-flip-horizontal'),
+					events: {
+						click: () => {
+							this.cropper.scaleX(-this.cropper.getData().scaleX || -1);
+						}
+					},
+					inside: $('.cropper-bar', popup)
+				});
+				$.create('button', {
+					type: 'button',
+					className: 'cropper-flip-vertical',
+					textContent: this.mavo._('cropper-flip-vertical'),
+					title: this.mavo._('cropper-flip-vertical'),
+					events: {
+						click: () => {
+							this.cropper.scaleY(-this.cropper.getData().scaleY || -1);
+						}
+					},
+					inside: $('.cropper-bar', popup)
+				});
+			}
+
+			// Modify the behaviour of the default editor: we don't want to upload an image
+			// before it is edited
+
+			// Do I have to unbind to change the event handler, or there is another way?
+			// What about other events: copy-paste, drag & drop?
+			$.unbind($('input[type=file]', popup), 'change');
+			$('input[type=file]', popup).addEventListener('change', evt => {
+				const file = evt.target.files[0];
+
+				if (file) {
+					fileName = file.name;
+					const tempURL = URL.createObjectURL(file);
+					this.element.setAttribute(this.attribute, tempURL);
+					if (this.cropper) {
+						this.cropper.replace(tempURL);
+					}
+				}
+			});
+			$.unbind(popup, 'drop');
+			$.bind(popup, {
+				'paste': evt => {},
+				'drop': evt => {
+					const file = evt.dataTransfer.files[0];
+
+					if (file) {
+						fileName = file.name;
+						const tempURL = URL.createObjectURL(file);
+						this.element.setAttribute(this.attribute, tempURL);
+						if (this.cropper) {
+							this.cropper.replace(tempURL);
+						}
+					}
+					popup.classList.remove('mv-dragover');
+					evt.preventDefault();
+				}
+			});
+
+			// Show an end user the editor
 			return popup;
 		},
 
-		// done: function () {
-		// 	if (this.cropper) {
-		// 		this.cropper.destroy();
-		// 	}
-		// }
+		done: function () {
+			// this.cropper.destroy();
+		}
 	});
+
+	Mavo.Locale.register('en', {
+		'cropper-upload': 'Upload',
+		'cropper-rotate-left': 'Rotate Left',
+		'cropper-rotate-right': 'Rotate Right',
+		'cropper-flip-horizontal': 'Flip Horizontal',
+		'cropper-flip-vertical': 'Flip Vertical'
+	});
+
 })(Bliss, Bliss.$);
