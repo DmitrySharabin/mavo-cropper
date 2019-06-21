573c6073230816b06904539b49447f92c90039ca
Dmitry Sharabin <dmitrysharabin@gmail.com>
2019-06-21 10:11:34 +0300

    fix: Apply the plugin only for images

--- src/mavo-cropper.js
+++ src/mavo-cropper.js
@@ -1,5 +1,4 @@
 (function ($) {
-	const SELECTOR = '.cropper, [mv-cropper-options]';
 
 	const defaults = {
 		viewMode: 2,
@@ -12,245 +11,232 @@
 			'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.1/cropper.min.css',
 			'mv-cropper.css'
 		],
-		extend: {
-			Elements: {
-				'media': {
-					default: true,
-					selector: 'img',
-					attribute: 'src',
-					// Without the editor function, it doesn't work as I expect:
-					// the simple popup appears. Why is that?
-					editor: function () {
-						Mavo.setAttributeShy(this.element, 'mv-uploads', 'images');
-						return this.createUploadPopup('image/*', 'image', 'png');
-					}
-				}
-			}
-		},
 		hooks: {
 			// TODO: Modify the behaviour of the default editor: we don't want to upload an image
 			// before it is edited
 			'primitive-createuploadpopup-beforereturn': function (env) {
-				const self = this;
+				if (env.kind === 'image') {
+					const self = this;
 
-				this.options = this.element.getAttribute('mv-cropper-options');
+					this.options = this.element.getAttribute('mv-cropper-options');
 
-				if (this.options) {
-					this.options = $.extend(defaults, Mavo.options(this.options));
-				} else {
-					this.options = defaults;
-				}
-				if (typeof this.data !== 'undefined') {
-					this.fileName = this.data.split('/').pop();
-					this.fileType = 'image/' + (this.fileName.split('.')[1] === 'png' ? 'png' : 'jpeg');
-
-					env.popup.classList.remove('cropper-no-image');
-				} else {
-					// What if there is no image? No problem, just hide the preview and the toolbar
-					env.popup.classList.add('cropper-no-image');
-				}
-
-				// Listen to every change of the source image
-				// and update the preview accordingly
-				// That's a bit slow. Could we fix that?
-				this.element.addEventListener('mv-change', evt => {
-					if (evt.value !== '') {
-						this.fileName = evt.value.split('/').pop();
+					if (this.options) {
+						this.options = $.extend(defaults, Mavo.options(this.options));
+					} else {
+						this.options = defaults;
+					}
+					if (typeof this.data !== 'undefined') {
+						this.fileName = this.data.split('/').pop();
 						this.fileType = 'image/' + (this.fileName.split('.')[1] === 'png' ? 'png' : 'jpeg');
 
-						$('.cropper-preview', env.popup).style.maxWidth = this.element.offsetWidth + 'px';
-						this.cropper.replace(evt.value);
-
-						// TODO: We need to update the toolbar according to the cropper options
 						env.popup.classList.remove('cropper-no-image');
 					} else {
+						// What if there is no image? No problem, just hide the preview and the toolbar
 						env.popup.classList.add('cropper-no-image');
 					}
-				});
 
-				// Extend the default editor with appropriate elements:
-				// a preview and a toolbar
-				$.create('div', {
-					// Wrap the image element with a block element (container)
-					className: 'cropper-wrapper',
-					contents: [{
-						tag: 'img',
-						src: this.data || '',
-						alt: this.mavo._('cropper-image-preview'),
-						className: 'cropper-preview',
-						style: {
-							maxWidth: this.element.offsetWidth + 'px'
+					// Listen to every change of the source image
+					// and update the preview accordingly
+					// That's a bit slow. Could we fix that?
+					this.element.addEventListener('mv-change', evt => {
+						if (evt.value !== '') {
+							this.fileName = evt.value.split('/').pop();
+							this.fileType = 'image/' + (this.fileName.split('.')[1] === 'png' ? 'png' : 'jpeg');
+
+							$('.cropper-preview', env.popup).style.maxWidth = this.element.offsetWidth + 'px';
+							this.cropper.replace(evt.value);
+
+							// TODO: We need to update the toolbar according to the cropper options
+							env.popup.classList.remove('cropper-no-image');
+						} else {
+							env.popup.classList.add('cropper-no-image');
 						}
-					}],
-					inside: env.popup
-				});
+					});
 
-				// Create the cropper
-				this.cropper = new Cropper($('.cropper-preview', env.popup), this.options);
-
-				// Generate the cropper-bar depending on the cropper options: this.cropper.options
-				$.create('div', {
-					className: 'cropper-bar',
-					contents: {
-						tag: 'button',
-						type: 'button',
-						className: 'cropper-upload',
-						title: this.mavo._('cropper-upload'),
-						events: {
-							click: () => {
-								this.cropper.getCroppedCanvas(
-									this.fileType === 'image/png' ? {} : {
-										fillColor: '#fff'
-									}
-								).toBlob(file => {
-									this.upload(file, this.fileName);
-								}, this.fileType);
-								// TODO: We need to update the toolbar according to the options the cropper had had before we uploaded the image
+					// Extend the default editor with appropriate elements:
+					// a preview and a toolbar
+					$.create('div', {
+						// Wrap the image element with a block element (container)
+						className: 'cropper-wrapper',
+						contents: [{
+							tag: 'img',
+							src: this.data || '',
+							alt: this.mavo._('cropper-image-preview'),
+							className: 'cropper-preview',
+							style: {
+								maxWidth: this.element.offsetWidth + 'px'
 							}
-						}
-					},
-					inside: env.popup
-				});
+						}],
+						inside: env.popup
+					});
 
-				// Crop
-				$.create('button', {
-					type: 'button',
-					className: 'cropper-crop',
-					events: {
-						click: function () {
-							this.classList.toggle('cropper-crop-hidden');
-							if (this.classList.contains('cropper-crop-hidden')) {
-								this.setAttribute('title', self.mavo._('cropper-show'));
-								$('.cropper-aspect-ratio', env.popup).style.visibility = 'hidden';
-								self.cropper.setDragMode();
-								self.cropper.clear();
-							} else {
-								this.setAttribute('title', self.mavo._('cropper-hide'));
-								$('.cropper-aspect-ratio', env.popup).style.visibility = 'visible';
-								self.cropper.setDragMode('crop');
-								self.cropper.crop();
+					// Create the cropper
+					this.cropper = new Cropper($('.cropper-preview', env.popup), this.options);
+
+					// Generate the cropper-bar depending on the cropper options: this.cropper.options
+					$.create('div', {
+						className: 'cropper-bar',
+						contents: {
+							tag: 'button',
+							type: 'button',
+							className: 'cropper-upload',
+							title: this.mavo._('cropper-upload'),
+							events: {
+								click: () => {
+									this.cropper.getCroppedCanvas(
+										this.fileType === 'image/png' ? {} : {
+											fillColor: '#fff'
+										}
+									).toBlob(file => {
+										this.upload(file, this.fileName);
+									}, this.fileType);
+									// TODO: We need to update the toolbar according to the options the cropper had had before we uploaded the image
+								}
 							}
-						}
-					},
-					inside: $('.cropper-bar', env.popup)
-				});
+						},
+						inside: env.popup
+					});
 
-				// Aspect Ratio
-				$.create('select', {
-					className: 'cropper-aspect-ratio',
-					title: this.mavo._('cropper-aspect-ratio'),
-					contents: [{
-							tag: 'option',
-							value: NaN,
-							textContent: 'Free'
-						},
-						{
-							tag: 'option',
-							value: 1,
-							textContent: '1:1'
-						},
-						{
-							tag: 'option',
-							value: 4 / 3,
-							textContent: '4:3'
-						},
-						{
-							tag: 'option',
-							value: 16 / 9,
-							textContent: '16:9'
-						},
-						{
-							tag: 'option',
-							value: 2 / 3,
-							textContent: '2:3'
-						}
-					],
-					events: {
-						change: evt => {
-							this.cropper.setAspectRatio(evt.target.value);
-						}
-					},
-					inside: $('.cropper-bar', env.popup)
-				});
-
-				// Rotate
-				if (this.cropper.options.rotatable) {
+					// Crop
 					$.create('button', {
 						type: 'button',
-						className: 'cropper-rotate-left',
-						title: this.mavo._('cropper-rotate-left'),
-						events: {
-							click: () => {
-								this.cropper.rotate(-90);
-							}
-						},
-						inside: $('.cropper-bar', env.popup)
-					});
-					$.create('button', {
-						type: 'button',
-						className: 'cropper-rotate-right',
-						title: this.mavo._('cropper-rotate-right'),
-						events: {
-							click: () => {
-								this.cropper.rotate(90);
-							}
-						},
-						inside: $('.cropper-bar', env.popup)
-					});
-				}
-
-				// Flip
-				if (this.cropper.options.scalable) {
-					$.create('button', {
-						type: 'button',
-						className: 'cropper-flip-horizontal',
-						title: this.mavo._('cropper-flip-horizontal'),
-						events: {
-							click: () => {
-								this.cropper.scaleX(-this.cropper.getData().scaleX || -1);
-							}
-						},
-						inside: $('.cropper-bar', env.popup)
-					});
-					$.create('button', {
-						type: 'button',
-						className: 'cropper-flip-vertical',
-						title: this.mavo._('cropper-flip-vertical'),
-						events: {
-							click: () => {
-								this.cropper.scaleY(-this.cropper.getData().scaleY || -1);
-							}
-						},
-						inside: $('.cropper-bar', env.popup)
-					});
-				}
-
-				// Tune the cropper bar according to the cropper options
-				if (this.cropper.options.autoCrop) {
-					$.set($('.cropper-crop', env.popup), {
 						className: 'cropper-crop',
-						title: this.mavo._('cropper-hide'),
+						events: {
+							click: function () {
+								this.classList.toggle('cropper-crop-hidden');
+								if (this.classList.contains('cropper-crop-hidden')) {
+									this.setAttribute('title', self.mavo._('cropper-show'));
+									$('.cropper-aspect-ratio', env.popup).style.visibility = 'hidden';
+									self.cropper.setDragMode();
+									self.cropper.clear();
+								} else {
+									this.setAttribute('title', self.mavo._('cropper-hide'));
+									$('.cropper-aspect-ratio', env.popup).style.visibility = 'visible';
+									self.cropper.setDragMode('crop');
+									self.cropper.crop();
+								}
+							}
+						},
+						inside: $('.cropper-bar', env.popup)
 					});
-					$.set($('.cropper-aspect-ratio', env.popup), {
-						// Need to set corresponding value (according to the aspectRatio parameter),
-						// but there are too many cases. Just ignore this parameter for now.
-						// What if we simply add another option—User Defined?
-						style: {
-							visibility: 'visible'
-						}
+
+					// Aspect Ratio
+					$.create('select', {
+						className: 'cropper-aspect-ratio',
+						title: this.mavo._('cropper-aspect-ratio'),
+						contents: [{
+								tag: 'option',
+								value: NaN,
+								textContent: 'Free'
+							},
+							{
+								tag: 'option',
+								value: 1,
+								textContent: '1:1'
+							},
+							{
+								tag: 'option',
+								value: 4 / 3,
+								textContent: '4:3'
+							},
+							{
+								tag: 'option',
+								value: 16 / 9,
+								textContent: '16:9'
+							},
+							{
+								tag: 'option',
+								value: 2 / 3,
+								textContent: '2:3'
+							}
+						],
+						events: {
+							change: evt => {
+								this.cropper.setAspectRatio(evt.target.value);
+							}
+						},
+						inside: $('.cropper-bar', env.popup)
 					});
-					this.cropper.setDragMode(this.cropper.options.dragMode);
-				} else {
-					$.set($('.cropper-crop', env.popup), {
-						className: 'cropper-crop cropper-crop-hidden',
-						title: this.mavo._('cropper-show'),
-					});
-					$('.cropper-aspect-ratio', env.popup).style.visibility = 'hidden';
-					// That's weird: this.cropper.setDragMode() doesn't work in that case.
-					// I have to use this workaround
-					$.extend(this.cropper.options, {
-						dragMode: 'none'
-					});
+
+					// Rotate
+					if (this.cropper.options.rotatable) {
+						$.create('button', {
+							type: 'button',
+							className: 'cropper-rotate-left',
+							title: this.mavo._('cropper-rotate-left'),
+							events: {
+								click: () => {
+									this.cropper.rotate(-90);
+								}
+							},
+							inside: $('.cropper-bar', env.popup)
+						});
+						$.create('button', {
+							type: 'button',
+							className: 'cropper-rotate-right',
+							title: this.mavo._('cropper-rotate-right'),
+							events: {
+								click: () => {
+									this.cropper.rotate(90);
+								}
+							},
+							inside: $('.cropper-bar', env.popup)
+						});
+					}
+
+					// Flip
+					if (this.cropper.options.scalable) {
+						$.create('button', {
+							type: 'button',
+							className: 'cropper-flip-horizontal',
+							title: this.mavo._('cropper-flip-horizontal'),
+							events: {
+								click: () => {
+									this.cropper.scaleX(-this.cropper.getData().scaleX || -1);
+								}
+							},
+							inside: $('.cropper-bar', env.popup)
+						});
+						$.create('button', {
+							type: 'button',
+							className: 'cropper-flip-vertical',
+							title: this.mavo._('cropper-flip-vertical'),
+							events: {
+								click: () => {
+									this.cropper.scaleY(-this.cropper.getData().scaleY || -1);
+								}
+							},
+							inside: $('.cropper-bar', env.popup)
+						});
+					}
+
+					// Tune the cropper bar according to the cropper options
+					if (this.cropper.options.autoCrop) {
+						$.set($('.cropper-crop', env.popup), {
+							className: 'cropper-crop',
+							title: this.mavo._('cropper-hide'),
+						});
+						$.set($('.cropper-aspect-ratio', env.popup), {
+							// Need to set corresponding value (according to the aspectRatio parameter),
+							// but there are too many cases. Just ignore this parameter for now.
+							// What if we simply add another option—User Defined?
+							style: {
+								visibility: 'visible'
+							}
+						});
+						this.cropper.setDragMode(this.cropper.options.dragMode);
+					} else {
+						$.set($('.cropper-crop', env.popup), {
+							className: 'cropper-crop cropper-crop-hidden',
+							title: this.mavo._('cropper-show'),
+						});
+						$('.cropper-aspect-ratio', env.popup).style.visibility = 'hidden';
+						// That's weird: this.cropper.setDragMode() doesn't work in that case.
+						// I have to use this workaround
+						$.extend(this.cropper.options, {
+							dragMode: 'none'
+						});
+					}
 				}
 			}
 		}
